6ffb0c803d14cf773cf3da6c2cd0fd70e8634a35
ivinercassio <ivinercassio@gmail.com>
2026-07-29 17:41:23 -0300

    settings router and create first page

---
+++ src/pages/Home.tsx
@@ -0,0 +1,10 @@
+
+export default function Home() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen gap-4">
+      <h1 className="text-3xl font-bold tracking-tight">Bem-vindo à Home!</h1>
+      <p className="text-muted-foreground">Sua estrutura com Tailwind e shadcn está pronta.</p>
+      <button>Clique aqui</button>
+    </div>
+  )
+}
